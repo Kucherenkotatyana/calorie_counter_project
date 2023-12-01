31975cff34bfb6b652436e22aceaa02bce40ac8a
Tatyana <kucherenkotatyana97@gmail.com>
2023-12-01 13:32:59 +0200

    CC-25: created daily statistics endpoint.

--- customer_profile/views.py
+++ customer_profile/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import get_object_or_404
+from django.db import models
+
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -10,6 +12,10 @@ from .serializers import (
     CustomerProfileUpdateSerializer,
 )
 from users.models import Customer
+from meal.models import Meal
+from activity.models import CustomerActivity
+
+from datetime import date, datetime
 
 
 class CustomerProfileView(APIView):
@@ -105,3 +111,113 @@ class CustomerProfileView(APIView):
 
         current_profile.delete()
         return Response({"Deleted."}, status=status.HTTP_204_NO_CONTENT)
+
+
+class DailyStatisticsView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        customer_id = request.user.id
+
+        target = self.get_target(customer_id)
+        total_calories = self.get_total_calories(customer_id)
+        total_activity = self.get_total_activity(customer_id)
+        calories_including_activity = self.get_calories_including_activity(
+            total_calories,
+            total_activity
+        )
+        percentage = self.get_percentage(calories_including_activity, target)
+        response = self.create_response(
+            target,
+            total_calories,
+            total_activity,
+            calories_including_activity,
+            percentage,
+        )
+
+        return Response(response, status=status.HTTP_200_OK)
+
+    def get_target(self, customer_id):
+        try:
+            current_profile = get_object_or_404(
+                CustomerProfile,
+                customer=customer_id
+            )
+            target = current_profile.target
+            return target
+        except (ValueError, Exception):
+            return "target wasn't set"
+
+    def get_total_calories(self, customer_id):
+        given_date = self.get_date_from_request()
+
+        customer_meals = Meal.objects.filter(
+            user=customer_id,
+            date_add__year=given_date.year,
+            date_add__month=given_date.month,
+            date_add__day=given_date.day,
+        )
+
+        # total_calories = customer_meals.aggregate(
+        #     total_calories=models.Sum('portion_calories') or 0
+        # )
+
+        total_calories = 0
+
+        for meal in customer_meals:
+            total_calories += int(meal.portion_calories)
+        return total_calories
+
+    def get_total_activity(self, customer_id):
+        given_date = self.get_date_from_request()
+        activities = CustomerActivity.objects.filter(
+            customer=customer_id,
+            date_add__date=given_date
+        )
+        # total_activity = activities.aggregate(
+        #     total_calories=models.Sum('spent_calories') or 0
+        # )
+
+        total_activity = 0
+        for activity in activities:
+            total_activity += int(activity.spent_calories)
+        return total_activity
+
+    def get_date_from_request(self):
+        input_date_add = self.request.query_params.get('date_add', None)
+
+        if input_date_add:
+            given_date = datetime.strptime(input_date_add, '%Y-%m-%d')
+        else:
+            given_date = date.today()
+        return given_date
+
+    def get_calories_including_activity(
+            self,
+            total_calories,
+            total_activity
+    ):
+        calories_including_activity = total_calories - total_activity
+        return calories_including_activity
+
+    def get_percentage(self, calories_including_activity, target):
+        percentage = int(calories_including_activity*100/target)
+        return percentage
+
+    def create_response(
+            self,
+            target,
+            total_calories,
+            total_activity,
+            calories_including_activity,
+            percentage,
+    ):
+        response = dict(
+            target=target,
+            total_calories=total_calories,
+            total_activity=total_activity,
+            calories_including_activity=calories_including_activity,
+            percentage=percentage,
+        )
+
+        return response
